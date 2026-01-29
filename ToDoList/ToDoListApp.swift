c4062418cd704ddce8f573de036745420b33f5cc
Prajakta Pathak <prajakta.coding.email@gmail.com>
2026-01-29 15:12:00 -0500

    Initial Commit

---
+++ ToDoList/ToDoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoListApp.swift
+//  ToDoList
+//
+//  Created by Prajakta Pathak on 1/29/26.
+//
+
+import SwiftUI
+
+@main
+struct ToDoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
