e6b6e0484f972cc386bddaea57cb6e9bed4db7d7
Prajakta Pathak <prajakta.coding.email@gmail.com>
2026-02-05 10:24:34 -0500

    Added empty list view when there are no items in the list.

---
+++ ToDoList/Views/NoItemsView.swift
@@ -0,0 +1,18 @@
+//
+//  NoItemsView.swift
+//  ToDoList
+//
+//  Created by Prajakta Pathak on 2/5/26.
+//
+
+import SwiftUI
+
+struct NoItemsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    NoItemsView()
+}
